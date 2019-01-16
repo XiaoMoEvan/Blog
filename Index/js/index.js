067e95a5a2a6ebe0c2ec585f3ca55178256a8c22
tzxiaomo <1431128779@qq.com>
2019-01-16 21:57:54 +0800

    博客主页导航栏目代码

---
+++ Index/js/index.js
@@ -0,0 +1,28 @@
+$(".index-nav-more").click(function() {
+    let _$this = $(this);
+    let _$thisParent = _$this.parent();
+    let _$navItemMore = _$thisParent.find(".index-nav-items-more");
+
+    let _hasClass = _$navItemMore.hasClass("index-nav-items-more-hide");
+    if (_hasClass) {
+        _$navItemMore.removeClass("index-nav-items-more-hide").addClass("index-nav-items-more-show");
+        //设置二级导航的宽度
+        _$thisParent.find(".index-nav-items-more").css({
+            "min-width": _$thisParent.width() + 60
+        });
+        bindCancelItemsMoreShow();
+
+    } else {
+        _$navItemMore.removeClass("index-nav-items-more-show").addClass("index-nav-items-more-hide");
+    }
+})
+
+var bindCancelItemsMoreShow = function() {
+    $(".index-nav-items-more-show").off("mouseleave").on("mouseleave", function() {
+        $(this).removeClass("index-nav-items-more-show").addClass("index-nav-items-more-hide");
+    })
+}
+
+$("body").css({
+    height: $(window).height()
+})
